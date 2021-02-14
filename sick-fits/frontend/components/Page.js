431295ef16c3cc5a84f486a8df401ff9eea835e7
John Graham <johnegraham2@gmail.com>
2021-02-14 17:12:58 -0500

    more styling

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,6 @@
 import {PropTypes} from 'prop-types';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
-import styled from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
     @font-face {
