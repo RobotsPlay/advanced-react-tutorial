34a622be4d044fbb1fae2be2442ff4f2c3bff24e
John Graham <johnegraham2@gmail.com>
2021-02-15 10:28:38 -0500

    added auth backend, added product schema'

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,30 +1,54 @@
 import 'dotenv/config';
-import {config, createSchema} from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorail';
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorail';
 
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 30,
-    secret: process.env.COOKIE_SECRET
-}
+  maxAge: 60 * 60 * 24 * 30,
+  secret: process.env.COOKIE_SECRET,
+};
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add roles here
+  },
+});
+
+export default withAuth(
+  config({
     server: {
-        cors: {
-            origin: [process.env.FRONTEND_URL]
-        }
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+      },
     },
     db: {
-        adapter: 'mongoose',
-        url: databaseURL,
-        // TODO: add data seeding
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data seeding
     },
     lists: createSchema({
-        // TODO: schema items here
+      User,
+      Product,
+      // TODO: schema items here
     }),
     ui: {
-        // TODO: change this for roles
-        isAccessAllowed: () => true,
+      // TODO: change this for roles
+      isAccessAllowed: ({ session }) => !!session?.data,
     },
-    // TODO: add session values here
-});
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
